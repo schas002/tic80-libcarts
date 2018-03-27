eaa415bf248b5b877a440bb7020e53b48293ff4a
Andrew Zyabin [🔒] <schas002@users.noreply.github.com>
2018-03-27 19:57:13 +0400

    :sparkles: add not much at all

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # tic80-libcarts
-Library carts for the TIC-80
+
+> Library carts for the TIC-80
+
+[TIC-80](https://tic.computer) is a tiny fantasy computer/game console.
+
+## The carts
+
+- [rawkey](rawkey.lua) - raw key packet
+
+## Maintainer
+
+- Andrew Zyabin - @schas002 - [@zyabin101@botsin.space](https://botsin.space/@zyabin101)
+
+## License
+
+[MIT](LICENSE) &copy; Andrew Zyabin
--- rawkey.lua
+++ rawkey.lua
@@ -1,3 +1,19 @@
+-- title: rawkey
+-- version: 1.0.0
+-- author: Andrew Zyabin
+-- desc: a library cart
+-- input: keyboard
+-- script: lua
+-- license: MIT
+
+--[[
+This is a library cartridge which code
+you can use in your own games for the
+TIC. The MIT License for that is
+included below.
+]]--
+
+--[[
 MIT License
 
 Copyright (c) 2018-2018, Andrew Zyabin
@@ -33,3 +49,28 @@ CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS
 IN THE SOFTWARE.
+]]--
+
+--[[
+You could include the License above in
+your cart's source code, but that's
+unwieldy. This cart weighs 1956/65536
+bytes, after all.
+
+You can also attach the License with
+the cart, in a file called
+LICENSE-rawkey or something.
+]]--
+
+-- | Return a raw key packet (little-endian) from the TIC.
+function rawkey()
+	return peek(0xff8b) << 24 | peek(0xff8a) << 16 | peek(0xff89) << 8 | peek(0xff88)
+end
+
+function TIC()
+	cls(1)
+	print('This is a library cartridge.', 8, 8)
+	print('Go ahead and use it! :)', 8, 16)
+	print('Demo: press any key', 8, 32, 14)
+	print('rawkey() == ' .. rawkey(), 8, 40)
+end
